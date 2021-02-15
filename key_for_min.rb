1e53a1b36dfebee3927a94cf03de9eb85443c595
Ashley Salazar <ashleysalazar0902@gmail.com>
2021-02-14 23:08:53 -0600

    commit tree e488504dae4661754dc6ab822ca27a9c5827c576

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,22 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+    return nil if name_hash.empty?
 
+    found_key = ""
+    curr_value = 0
+
+    name_hash.each_with_index do |(key, value), i|
+        if i == 0
+            curr_value = value
+        end
+        
+        if value < curr_value
+            curr_value = value
+        end
+    end
+
+    found_key = name_hash.index(curr_value)
+
+    return found_key
 end
